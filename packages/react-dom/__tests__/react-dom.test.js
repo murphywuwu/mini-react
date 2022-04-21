a9b30d00ea6daecec445456b7eed6e2e4d939fae
murphywuwu <murphywuwu@gmail.com>
2022-04-22 03:59:24 +0800

    :construction: ({) 支持渲染虚拟DOM

--- packages/react-dom/__tests__/react-dom.test.js
+++ packages/react-dom/__tests__/react-dom.test.js
@@ -1,7 +1,34 @@
-'use strict';
-
-const reactDom = require('..');
+const ReactDOM = require('..')
+const React = require('../../react')
 
 describe('react-dom', () => {
-    it('needs tests');
-});
+  test('hello world', function () {
+    const root = document.createElement('root')
+
+    const element = React.createElement('h1', {}, 'hello, world')
+
+    ReactDOM.render(element, root)
+
+    expect(root.innerHTML).toBe('<h1>hello, world</h1>')
+  })
+
+  test('list', function () {
+    const root = document.createElement('root')
+    const element = React.createElement(
+      'ul',
+      {
+        style: {
+          color: 'white',
+        },
+      },
+      React.createElement('li', { style: { background: 'blue' } }, '1'),
+      React.createElement('li', { style: { background: 'pink' } }, '2'),
+      React.createElement('li', { style: { background: 'black' } }, '3'),
+    )
+
+    ReactDOM.render(element, root)
+    expect(root.innerHTML).toBe(
+      '<ul style="color: white;"><li style="background: blue;">1</li><li style="background: pink;">2</li><li style="background: black;">3</li></ul>',
+    )
+  })
+})
--- packages/react-dom/lib/react-dom.js
+++ packages/react-dom/lib/react-dom.js
@@ -1,7 +1,52 @@
-'use strict';
+function renderDOM(element) {
+  const { type, props } = element
+  const children = props.children
 
-module.exports = reactDom;
+  // 第一步创建dom
+  const node = document.createElement(type)
 
-function reactDom() {
-    // TODO
+  // 第二步为dom添加属性
+  Object.keys(props).forEach((key) => {
+    if (key == 'children') return
+    let val = props[key]
+
+    // 支持style属性
+    if (key == 'style') {
+      let str = ''
+      Object.keys(val).forEach((key) => {
+        str += `${key}: ${val[key]};`
+      })
+      val = str
+    }
+
+    node.setAttribute(key, val)
+  })
+
+  // 第三步为dom添加children
+  if (typeof children == 'string') {
+    const childNode = document.createTextNode(children)
+
+    node.appendChild(childNode)
+  }
+
+  // 支持复杂虚拟DOM树
+  if (Array.isArray(children)) {
+    children.forEach((element) => {
+      const childNode = renderDOM(element)
+      node.appendChild(childNode)
+    })
+  }
+
+  return node
+}
+
+function render(element, root) {
+  const node = renderDOM(element)
+
+  // 最后，在root中插入dom
+  root.appendChild(node)
+}
+
+module.exports = {
+  render,
 }
