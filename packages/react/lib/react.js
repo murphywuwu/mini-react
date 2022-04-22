4cda60c7e32046e0c4e7817ae6c68cfbbeea61a5
murphywuwu <murphywuwu@gmail.com>
2022-04-22 16:52:44 +0800

    :construction:  support function component

--- packages/react/lib/react.js
+++ packages/react/lib/react.js
@@ -1,5 +1,14 @@
 module.exports = {
   createElement: function (type, props, ...params) {
+    if (typeof type == 'function') {
+      return {
+        type,
+        props: {
+          ...props,
+        },
+      }
+    }
+
     let children = params
     if (params.length == 1) {
       children = params[0]
