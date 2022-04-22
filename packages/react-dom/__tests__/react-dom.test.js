d935d9fde9aaffb0975201de0949d3e8c9b773f8
murphywuwu <murphywuwu@gmail.com>
2022-04-22 16:53:31 +0800

    :white_check_mark: (packages/react-dom/__tests__/react-dom.test.js)
    support render function component for simple situation

--- packages/react-dom/__tests__/react-dom.test.js
+++ packages/react-dom/__tests__/react-dom.test.js
@@ -2,33 +2,51 @@ const ReactDOM = require('..')
 const React = require('../../react')
 
 describe('react-dom', () => {
-  test('hello world', function () {
-    const root = document.createElement('root')
+  describe('render', function () {
+    test('hello world', function () {
+      const root = document.createElement('root')
 
-    const element = React.createElement('h1', {}, 'hello, world')
+      const element = React.createElement('h1', {}, 'hello, world')
 
-    ReactDOM.render(element, root)
+      ReactDOM.render(element, root)
 
-    expect(root.innerHTML).toBe('<h1>hello, world</h1>')
-  })
+      expect(root.innerHTML).toBe('<h1>hello, world</h1>')
+    })
 
-  test('list', function () {
-    const root = document.createElement('root')
-    const element = React.createElement(
-      'ul',
-      {
-        style: {
-          color: 'white',
+    test('list', function () {
+      const root = document.createElement('root')
+      const element = React.createElement(
+        'ul',
+        {
+          style: {
+            color: 'white',
+          },
         },
-      },
-      React.createElement('li', { style: { background: 'blue' } }, '1'),
-      React.createElement('li', { style: { background: 'pink' } }, '2'),
-      React.createElement('li', { style: { background: 'black' } }, '3'),
-    )
+        React.createElement('li', { style: { background: 'blue' } }, '1'),
+        React.createElement('li', { style: { background: 'pink' } }, '2'),
+        React.createElement('li', { style: { background: 'black' } }, '3'),
+      )
 
-    ReactDOM.render(element, root)
-    expect(root.innerHTML).toBe(
-      '<ul style="color: white;"><li style="background: blue;">1</li><li style="background: pink;">2</li><li style="background: black;">3</li></ul>',
-    )
+      ReactDOM.render(element, root)
+      expect(root.innerHTML).toBe(
+        '<ul style="color: white;"><li style="background: blue;">1</li><li style="background: pink;">2</li><li style="background: black;">3</li></ul>',
+      )
+    })
+
+    describe('support function component', function () {
+      test('simple', function () {
+        const root = document.createElement('root')
+        function Welcome(props) {
+          return React.createElement('h1', null, 'hello, ', props.name)
+        }
+        const element = React.createElement(Welcome, { name: 'world' })
+
+        ReactDOM.render(element, root)
+
+        expect(root.innerHTML).toBe('<h1>hello, world</h1>')
+      })
+
+      test('composite', function () {})
+    })
   })
 })
