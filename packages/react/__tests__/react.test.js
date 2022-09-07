c17c35276c985a22e672e8d28eb418088c47a0e6
murphywuwu <murphywuwu@gmail.com>
2022-09-07 14:38:31 +0800

    :fire: (packages/react/__tests__/react.test.js) rm list test

--- packages/react/__tests__/react.test.js
+++ packages/react/__tests__/react.test.js
@@ -13,58 +13,6 @@ describe('react', () => {
         },
         type: 'h1',
       })
-    })
-
-    test('list', function () {
-      const result = React.createElement(
-        'ul',
-        {
-          style: {
-            color: 'white',
-          },
-        },
-        React.createElement('li', { style: { background: 'blue' } }, '1'),
-        React.createElement('li', { style: { background: 'pink' } }, '2'),
-        React.createElement('li', { style: { background: 'black' } }, '3'),
-      )
-
-      expect(result).toEqual({
-        type: 'ul',
-        props: {
-          children: [
-            {
-              type: 'li',
-              props: {
-                children: '1',
-                style: {
-                  background: 'blue',
-                },
-              },
-            },
-            {
-              type: 'li',
-              props: {
-                children: '2',
-                style: {
-                  background: 'pink',
-                },
-              },
-            },
-            {
-              type: 'li',
-              props: {
-                children: '3',
-                style: {
-                  background: 'black',
-                },
-              },
-            },
-          ],
-          style: {
-            color: 'white',
-          },
-        },
-      })
-    })
-  })
-})
+    });
+  });
+});
