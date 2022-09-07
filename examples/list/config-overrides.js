be81640700a7c3c48d9c0e153c618ca8ae7235f8
murphywuwu <murphywuwu@gmail.com>
2022-09-07 12:36:42 +0800

    :wrench: (examples/list/config-overrides.js) add alias for @pacakges

--- examples/list/config-overrides.js
+++ examples/list/config-overrides.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = function override(config, env) {
-  const { module } = config;
+  const { module, resolve } = config;
   const { rules } = module;
+  const { alias, plugins } = resolve;
   
   config.module.rules = [
     ...rules,
@@ -12,7 +15,18 @@ module.exports = function override(config, env) {
         presets: ["@babel/preset-react"]
       }
     }
-  ]
+  ];
+
+  // this will remove the CRA plugin that prevents to import modules from 
+  // outside the `src`  directory, useful if you use a different directory
+  config.resolve.plugins = plugins.filter(
+    p => p.constructor.name !== "ModuleScopePlugin"
+  );
+
+  config.resolve.alias = {
+    '@packages': path.resolve(__dirname, '../../packages/'),
+    ...alias
+  }
 
   return config;
 }
