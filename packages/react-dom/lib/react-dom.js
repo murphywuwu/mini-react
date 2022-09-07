10aa12cba2cc65b17032c96a3fc2780bd23f51af
murphywuwu <murphywuwu@gmail.com>
2022-09-07 14:39:09 +0800

    :fire: (packages/) prepare for day1

--- packages/react-dom/lib/react-dom.js
+++ packages/react-dom/lib/react-dom.js
@@ -1,21 +1,5 @@
 function render(element, root) {
-  const { type, props } = element
-  const children = props.children
-
-  // 第一步创建dom
-  const node = document.createElement(type)
-
-  // 第二步为dom添加children
-  if (typeof children == 'string') {
-    // 创建节点
-    const childNode = document.createTextNode(children)
-    
-    // 插入节点
-    node.appendChild(childNode)
-  }
-
-  // 第三步在root中插入dom
-  root.appendChild(node)
+  
 }
 
 exports.render = render
--- packages/react/lib/react.js
+++ packages/react/lib/react.js
@@ -1,11 +1,5 @@
 function createElement(type, props, children) {
-  return {
-    type,
-    props: {
-      children,
-      ...props,
-    },
-  }
+  
 }
 
 exports.createElement = createElement;
