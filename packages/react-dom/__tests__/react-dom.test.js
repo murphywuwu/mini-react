e15c64c33a7306093c33f6adb27eaac93ad17a51
murphywuwu <murphywuwu@gmail.com>
2022-09-07 14:37:37 +0800

    :fire: (packages/react-dom/__tests__/react-dom.test.js) rm list test

--- packages/react-dom/__tests__/react-dom.test.js
+++ packages/react-dom/__tests__/react-dom.test.js
@@ -10,25 +10,5 @@ describe('react-dom', () => {
     ReactDOM.render(element, root)
 
     expect(root.innerHTML).toBe('<h1>hello, world</h1>')
-  })
-
-  test('list', function () {
-    const root = document.createElement('root')
-    const element = React.createElement(
-      'ul',
-      {
-        style: {
-          color: 'white',
-        },
-      },
-      React.createElement('li', { style: { background: 'blue' } }, '1'),
-      React.createElement('li', { style: { background: 'pink' } }, '2'),
-      React.createElement('li', { style: { background: 'black' } }, '3'),
-    )
-
-    ReactDOM.render(element, root)
-    expect(root.innerHTML).toBe(
-      '<ul style="color: white;"><li style="background: blue;">1</li><li style="background: pink;">2</li><li style="background: black;">3</li></ul>',
-    )
-  })
+  });
 })
